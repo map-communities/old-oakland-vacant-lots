6df5cc29cc0cf51bdc2ad505e5f63812b33409cb
Max Edmands <max@goodeggs.com>
2014-02-22 13:00:24 -0800

    Parse float instead of int

--- parsecsv.js
+++ parsecsv.js
@@ -11,8 +11,8 @@ csv()
       name: row[7].replace(/\s+/, ' '),
       street: row[8].replace(/\s+/, ' '),
       category: row[18],
-      lat: parseInt(row[20]),
-      lng: parseInt(row[21])
+      lat: parseFloat(row[20]),
+      lng: parseFloat(row[21])
     };
     if(index > 0 && index < 300) {
       vacantLots.push(lot);
