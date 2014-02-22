dcb7e1d7c132000ad2b2a6994336cd0355baf9e5
Max Edmands <max@goodeggs.com>
2014-02-22 12:47:47 -0800

    Quick program to parse csv data

---
+++ parsecsv.js
@@ -0,0 +1,25 @@
+var csv = require('csv');
+var fs = require('fs');
+var geojson = require('geojson');
+
+var vacantLots = []
+csv()
+  .from
+  .stream(fs.createReadStream('./vacants.csv'))
+  .transform(function(row, index) {
+    var lot = {
+      name: row[7].replace(/\s+/, ' '),
+      street: row[8].replace(/\s+/, ' '),
+      category: row[18],
+      lat: row[20],
+      lng: row[21]
+    };
+    if(index > 0) {
+      vacantLots.push(lot);
+    }
+  })
+  .on('end', function() {
+    geojson.parse(vacantLots, {Point: ['lat', 'lng']}, function(geojson) {
+      fs.writeFileSync('vacantlots.geojson', JSON.stringify(geojson));
+    });
+  });
