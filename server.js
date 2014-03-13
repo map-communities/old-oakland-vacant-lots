4bb42cd808c66e3ff07d43fb22060804c629f5ad
scrivy <scrivinator@gmail.com>
2014-03-12 19:13:01 -0700

    not working, but progress towards migrating to heroku

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 var express = require('express')
-  , fs = require('fs');
+  , MongoClient = require('mongodb').MongoClient
+  , Server = require('mongodb').Server;
 
+// express
 var app = express();
-
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.static(__dirname + '/public'));
+
+// mongodb
+var mongoclient = new MongoClient(new Server('oceanic.mongohq.com', 10045));
+mongoclient.open(function(err, mongoclient) {
+  if (err) console.log('Mongodb error - ' + err);
+
+  var db = mongoclient.db('app22981599');
+  db.authenticate('imauser', 'ldt4ieb72o684p4', function(err, data) {
+    if (err) console.log('authentication error - ' + err);
+    if (data) console.log('authenticated! - ' + data);
+  });
+
+  // collections
+  var lots = db.collection('lots');
+});
 
 // cache vacant lots in memory until a db is implemented
 var vacantlots = fs.readFileSync(__dirname + '/vacantlots2.json', { encoding: 'utf8' });
@@ -13,9 +30,6 @@ app.get('/vacantlots.json', function(req, res) {
   res.type('json');
   res.send(vacantlots);
 });
-
-// serve static files
-app.use(express.static(__dirname));
 
 // start listening
 app.listen('5000', function() {
