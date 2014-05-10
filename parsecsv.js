22395b59eb0bca875ef96549419d7fb75ae107e2
Max Edmands <me@maxedmands.com>
2014-05-10 15:07:33 -0700

    Update parsecsv script to srsly clean up data

--- parsecsv.js
+++ parsecsv.js
@@ -1,24 +1,71 @@
 var csv = require('csv');
 var fs = require('fs');
 var geojson = require('geojson');
+var path = require('path');
+var S = require('string');
+var titlecase = require('titlecase');
+var moment = require('moment-timezone');
+
+var key_fixes = {
+  OBJECTID: 'object_id',
+  DATE_CREAT: 'created_at',
+  DATE_UPDAT: 'updated_at',
+  OwnMail: 'owner_address',
+  OwnZip: 'owner_zip',
+  ZnLabel: 'zone_label',
+  Acre: 'acreage',
+  SqFt: 'square_footage'
+}
+
+var key_remove = [
+  'APN_SORT',
+  'APNJoin',
+  'Clean_Pa21',
+]
+
+function cleanKey(key) {
+  if(key_remove.indexOf(key) >= 0) {
+    return;
+  }
+
+  newKey = key_fixes[key] || S(key).underscore().s;
+  if(newKey[0] == '_') {
+    newKey = newKey.substr(1);
+  }
+  return newKey;
+}
+
+function fixAddress(address) {
+  var address = titlecase(address.toLowerCase());
+  return address.replace(' Ca', ', CA');
+}
+
+function clean(lot) {
+  var newLot = {};
+
+  Object.keys(lot).forEach(function(key) {
+    var newKey = cleanKey(key);
+    if(newKey) {
+      newLot[newKey] = lot[key];
+    }
+  });
+
+  newLot.address = fixAddress(newLot.address);
+  newLot.owner = fixAddress(newLot.owner);
+  newLot.owner_address = fixAddress(newLot.owner_address);
+  newLot.city = fixAddress(newLot.city);
+
+  newLot.created_at = moment.tz(newLot.created_at, 'America/Los_Angeles').toISOString()
+  newLot.updated_at = moment.tz(newLot.updated_at, 'America/Los_Angeles').toISOString()
+
+  return newLot;
+}
 
 var vacantLots = []
 csv()
-  .from
-  .stream(fs.createReadStream('./vacants.csv'))
-  .transform(function(row, index) {
-    var lot = {
-      name: row[7].replace(/\s+/, ' '),
-      street: row[8].replace(/\s+/, ' '),
-      category: row[18],
-      lng: parseFloat(row[20]),
-      lat: parseFloat(row[21]),
-      acres: parseFloat(row[25]),
-      sqFt: parseFloat(row[26])
-    };
-    if(index > 0) {
-      vacantLots.push(lot);
-    }
+  .from.path(path.join(__dirname, 'vacants.csv'), {columns: true})
+  .transform(function(lot) {
+    vacantLots.push(clean(lot));
   })
   .on('end', function() {
     geojson.parse(vacantLots, {Point: ['lat', 'lng']}, function(geojson) {
